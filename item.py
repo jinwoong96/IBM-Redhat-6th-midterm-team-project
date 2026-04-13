f2d0b5e8a0729a6f98c1a8d253ad06cf110fbd9f
jaebeombag638-cyber <jaebeombag638@gmail.com>
2026-04-13 18:05:00 +0900

    Fix indentation for Item class definition

--- item.py
+++ item.py
@@ -6,11 +6,11 @@ from typing import Optional, List, TYPE_CHECKING
 
 
 
-    class Item(Base):
-        __tablename__ = "items"
-        item_code: Mapped[str] = mapped_column(String(10),primary_key=True,nullable=False)
-        item_name: Mapped[str] = mapped_column(String(50),nullable=False)
-        item_category: Mapped[str] = mapped_column(String(50),nullable=False)
-        curr_price: Mapped[int] = mapped_column(Integer, nullable=False)
-        flu_range: Mapped[int] = mapped_column(Integer)
-        flu_range_percent: Mapped[float] = mapped_column(Float)
+class Item(Base):
+    __tablename__ = "items"
+    item_code: Mapped[str] = mapped_column(String(10),primary_key=True,nullable=False)
+    item_name: Mapped[str] = mapped_column(String(50),nullable=False)
+    item_category: Mapped[str] = mapped_column(String(50),nullable=False)
+    curr_price: Mapped[int] = mapped_column(Integer, nullable=False)
+    flu_range: Mapped[int] = mapped_column(Integer)
+    flu_range_percent: Mapped[float] = mapped_column(Float)
