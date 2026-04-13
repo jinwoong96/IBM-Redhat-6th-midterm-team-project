8afb9c732e3655df25ae82cecd03ea5a4b5d5089
jaebeom <jaebeombag638@gmail.com>
2026-04-13 17:45:53 +0900

    feat: Wish_list 테이블

---
+++ add/wish_list.py
@@ -0,0 +1,11 @@
+from backend.database import Base
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import datetime
+from sqlalchemy import String, TIMESTAMP, func, Integer, ForeignKey
+from typing import Optional, List, TYPE_CHECKING
+
+class Wish_list(Base):
+    __tablename__ = "wishlists"
+    wishlist_id: Mapped[int] = mapped_column(primary_key=True,nullable=False)
+    user_id: Mapped[str] = mapped_column(ForeignKey("users.user_id", ondelete="CASCADE"),String(50),nullable=False)
+    item_code: Mapped[str] = mapped_column(ForeignKey("ltems.item_code", ondelete="CASCADE"),String(10),nullable=False)
