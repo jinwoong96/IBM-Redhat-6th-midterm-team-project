1a0f99da5ac5b200679601535ed9c9ce2bf4cbe6
yoonho <leynord112@naver.com>
2026-04-22 21:23:30 +0900

    뉴스별 카테고리 등락 및 랜덤뉴스배정 완료

--- backend/app/services/news_user.py
+++ backend/app/services/news_user.py
@@ -7,6 +7,8 @@ from app.db.crud.news_user import NewsuserCrud
 from app.db.crud.effects import EffectsCrud
 from app.db.models.news import News
 import random
+from app.db.crud.chart_user import ChartuserCrud
+from app.db.scheme.chart_user import ChartuserUpdate
 class NewsuserService:
 
     @staticmethod #사용자의 과거뉴스기록 조회
@@ -35,6 +37,9 @@ class NewsuserService:
             raise HTTPException(status_code=404,detail='새로운 뉴스가 없습니다')#만약 뉴스가 더없다면..
         
         select_random_news=random.choice(unseen_news)#랜덤으로 뉴스를 뽑는다
+        res_news_id = select_random_news.news_id
+        res_news_title = select_random_news.news_title
+        res_news_comments = select_random_news.news_comments
 
         await NewsuserCrud.create(#새로 배정된 뉴스를 db에저장 근데 이거 다음날 뉴스라서 저장할때 +1이 맞지않나?(해결)
             login_id=login_id,
@@ -53,14 +58,53 @@ class NewsuserService:
                 'category_name':i.category_name,
                 'flu_value':effect_flu
             })
-        
-        
 
-        return {
-            "news_id": select_random_news.news_id,
-            "news_title": select_random_news.news_title,
-            "news_comments": select_random_news.news_comments,
-            "effects": category_effect
-        }
-        
+        user_current_stock=await ChartuserCrud.get_item_list_crud(login_id,db) #가장최신날짜의 종목들을 다 가져와서
+        for stock in user_current_stock:# 그 안에있는 종목들을 가져와서
+            apply_flu=None
+            for i in category_effect:#카테고리별 영향을 반복해서 돌린후
+                if i['category_name'] == stock['category_name']:#지정된 뉴스에 나온 종목이름과 전체종목이름중 같은게 있으면
+                    apply_flu=i['flu_value']#지정된 등락폭으로 확정
+                    break
+
+            if apply_flu is None:#영향없는 종목들은 -3~3퍼센트의 등락률 지정
+                apply_flu = round(random.uniform(-3.0, 3.0), 2)
+
+            next_start_price=stock['end_price']#다음날 시작가 
+            next_flu_range=round(next_start_price*(apply_flu/100))#다음날 전날대비 등락률
+            next_end_price=next_start_price+next_flu_range#다음날 종가
+
+            next_max_price = max(next_start_price,next_end_price)*(1+random.uniform(0,0.01))#종가나 시가 중 높은금액에 살짝 더높은 최고가
+            next_min_price = min(next_start_price,next_end_price)*(1-random.uniform(0,0.01))#종가나 시가 중 낮은금액에 살짝 더낮은 최저가
+
+            next_max_price = round(next_max_price)
+            next_min_price = round(next_min_price)
+
+            await ChartuserCrud.create_all(
+                user_id=login_id,
+                chart=ChartuserUpdate(
+                        item_code=stock['item_code'],
+                        start_price=next_start_price,
+                        end_price=next_end_price,
+                        max_price=next_max_price,
+                        min_price=next_min_price,
+                        day=next_day,
+                        flu_range=next_flu_range,
+                        flu_range_percent=apply_flu
+                    ),
+                    db=db
+                )
         await db.commit()
+
+        return{"news_id": res_news_id,
+                "news_title": res_news_title,
+                "news_comments": res_news_comments
+                }
+
+
+
+
+
+
+
+            
