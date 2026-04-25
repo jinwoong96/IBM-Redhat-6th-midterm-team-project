a229cd8e09b6689f183bfec68992cae9d4983865
Leegijun <dlrlwms11@gmail.com>
2026-04-25 18:13:25 +0900

    일차 넘기기 버튼빼고 완료

--- frontend/src/components/news/NewsItem.jsx
+++ frontend/src/components/news/NewsItem.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { fetchNewsUser } from '../../Slice/newsuserSlice';
 
 const NewsItem = () => {
     const dispatch = useDispatch();
+    const newslist = useSelector((state)=>state.newsuser.newslist)
+
+    useEffect(()=>{
+        dispatch(fetchNewsUser());
+    },[dispatch])
     return (
-        <div>
-            뉴스 제목, 내용 등 출력해주기
-        </div>
-    );
+    <div>
+        {newslist.length > 0 ? (
+            newslist.map((news) => (
+                <div key={news.news_id}>
+                    <h4>{news.news_title}</h4>
+                    <p>{news.news_comments}</p>
+                </div>
+            ))
+        ) : (
+            <p>뉴스가 없습니다.</p>
+        )}
+    </div>
+);
 };
 
 export default NewsItem;
